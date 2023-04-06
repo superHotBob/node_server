a8ba6e0ff56447175310d796dae7244220e5aa68
superHotBob <19ham09@gmail.com>
2023-04-06 09:36:32 +0300

    last_commit

--- index.js
+++ index.js
@@ -1,13 +1,57 @@
-const express = require('express'); //Import the express dependency
-const app = express();              //Instantiate an express app, the main work horse of this server
+const express = require('express'); 
+const app = express();              
 const port = process.env.PORT || 5000;
 const path = require('path')
+const fileUpload = require("express-fileupload");
+const fs = require('fs');
 
-app.get('/', (req, res) => {        //get requests to the root ("/") will route here
-    res.sendFile('index.html', {root: __dirname});      //server responds by sending the index.html file to the client's browser
-                                                        //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
+app.use(
+    fileUpload({
+        limits: {
+            fileSize: 1024 * 1024 // 1 MB
+        },
+        abortOnLimit: true
+     })
+);
+function ReadDir(req,res) {
+   
+    const directoryPath = path.join(__dirname + '/public' , 'images');
+    fs.readdir(directoryPath,{withFileTypes: true}, (err, files) => {
+        if (err) {
+            return console.log('Unable to scan directory: ' + err);
+        } 
+    let f = files.map(i=>i.name)
+    console.log(f);
+    res.send(f) 
+  })
+}
+
+app.get('/read',ReadDir, (req,res) => {
+   
+  
+})
+app.get('/', (req, res) => {        
+    res.sendFile('index.html', {root: __dirname});      
 });
+
+app.post("/upload", (req, res) => {
+    console.log(req.files.myfile);
+    if (!req.files) {
+      return res.status(400).send("No files were uploaded.");
+    }
+  
+    const file = req.files.myfile;
+    const path = __dirname + "/public/images/" + file.name;
+  
+    file.mv(path, (err) => {
+      if (err) {
+        return res.status(500).send(err);
+      }
+      return res.send({ status: "success", path: path });
+    });
+  });
+
 app.use(express.static(path.join(__dirname, '/public' )));
-app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
+app.listen(port, () => {           
     console.log(`Now listening on port ${port}`); 
 });
--- src/middlewary/utils.js
+++
@@ -1,4 +0,0 @@
-exports.ipfsLink = (hash) => {
-    // return `https://ipfs.io/ipfs/${hash}`;
-    return `https://nftrealworld.mypinata.cloud/ipfs/${hash}`;
-  }
